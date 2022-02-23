08aa740bb4924da9d57cc99a2d144ab7fe063c33
Sayed Riaydh <$email>
2022-02-23 09:20:02 +0600

    transaction list render

--- src/containers/Home/SummaryTable.js
+++ src/containers/Home/SummaryTable.js
@@ -3,7 +3,9 @@ import { useSelector } from 'react-redux';
 
 const SummaryTable = () => {
   const transaction = useSelector((state) => state.transaction.transactionList);
-  console.log(transaction);
+
+  let sum = [0, 0, 0, 0, 0, 0, 0, 0];
+
   return (
     <div className="main">
       <table
@@ -44,33 +46,54 @@ const SummaryTable = () => {
             <td width="150"> Account payable </td>
           </tr>
 
-          {transaction?.map((item) => (
-            <tr className="balancerow" bgcolor="#FFE0B1" align="center">
-              <td width="100"> {item.date} </td>
-              <td width="100"> Cash </td>
-              <td width="100"> Account Receivable </td>
-              <td width="100"> Supplies </td>
-              <td width="100"> Office equipment </td>
-              <td width="10"> = </td>
-              <td width="150"> Notes payable </td>
-              <td width="150"> Account payable </td>
-              <td width="150"> Revenue </td>
-            </tr>
-          ))}
+          {transaction?.map(({ date, balance, t1, tt1, t2, tt2 }) => {
+            let arr = ['', '', '', '', '', '', '', ''];
+            arr[t1 - 1] = balance * tt1;
+            arr[t2 - 1] = balance * tt2;
+            sum[t1 - 1] = sum[t1 - 1] + arr[t1 - 1];
+            sum[t2 - 1] = sum[t2 - 1] + arr[t2 - 1];
+
+            return (
+              <tr className="balancerow" bgcolor="#FFE0B1" align="center">
+                <td width="100"> {date} </td>
+                <td> {arr[0]}</td>
+                <td> {arr[1]}</td>
+                <td> {arr[2]}</td>
+                <td> {arr[3]}</td>
+                <td> = </td>
+                <td> {arr[4]}</td>
+                <td> {arr[5]}</td>
+                <td> {arr[6]}</td>
+              </tr>
+            );
+          })}
+
+          <tr className="balancerow" bgcolor="#FFE0B1" align="center">
+            <td width="100"></td>
+            <td> {sum[0]}</td>
+            <td> {sum[1]}</td>
+            <td> {sum[2]}</td>
+            <td> {sum[3]}</td>
+            <td> = </td>
+            <td> {sum[4]}</td>
+            <td> {sum[5]}</td>
+            <td> {sum[6]}</td>
+          </tr>
+
           <tr>
             <td>
               <h4>Grand Total</h4>
             </td>
             <td> </td>
             <td>
-              <h4> 100 TK</h4>
+              <h4> {sum[0] + sum[1] + sum[2] + sum[3]}</h4>
             </td>
             <td> </td>
             <td> </td>
             <td>=</td>
             <td> </td>
             <td>
-              <h4>100 TK</h4>
+              <h4>{sum[4] + sum[5] + sum[6]}</h4>
             </td>
             <td> </td>
           </tr>
